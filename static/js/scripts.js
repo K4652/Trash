9b5dcdf102c8a4454de716d5478ca706b6abfb03
K4652 <poiu4652@gmail.com>
2024-08-29 15:38:52 +0900

    Add files via upload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 /*!
 * Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
 // This file is intentionally blank
+=======
+/*!
+* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+>>>>>>> 35121715e30cee38204ba0f4a315bf3dc915d77f
 // Use this file to add JavaScript to your project
